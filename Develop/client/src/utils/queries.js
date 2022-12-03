3e672035f013f232a81d29597732f6d2ad4c1d96
Abdaleh Hersi <therealhersi03@gmail.com>
2022-12-03 22:02:55 +0000

    adding useQuery functionality

--- Develop/client/src/utils/queries.js
+++ Develop/client/src/utils/queries.js
@@ -1,9 +1,17 @@
 import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
-    query Me {
-    me {
-        _id
+query Me {
+  me {
+    _id
+    bookCount
+    savedBooks {
+      authors
+      description
+      title
+      image
+      link
     }
+  }
 }
 `;
